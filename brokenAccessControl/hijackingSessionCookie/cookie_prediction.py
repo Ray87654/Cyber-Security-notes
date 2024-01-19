05319fb8a33dd98dbd2da11289efbac8b390044e
James-Lu-none <jamesabcde277@gmail.com>
2024-01-19 18:07:55 +0800

    add file reader

--- brokenAccessControl/hijackingSessionCookie/cookie_prediction.py
+++ brokenAccessControl/hijackingSessionCookie/cookie_prediction.py
@@ -1,12 +1,11 @@
-jSESSIONID="dVD2iRJXLGbolu7uYc6frWr4vZYZuR-TjZ3LEFI5"
-hijack_cookie="2338673462221919916-1705652464016"
-1705652464016
-1705652464016
-import time
-for char in jSESSIONID:
-    print(ord(char),end=",")
-print("\n")
-for char in hijack_cookie:
-    print(ord(char),end=",")
-print("\n")  
-print(time.time())
+import os
+
+tokens=[]
+file_path="brokenAccessControl/hijackingSessionCookie/tokens"
+with open(file_path, 'r', encoding='utf-8') as file:
+    for line in file:
+        line = line.strip()
+        if not line: continue
+        seq, epoch_time = line.replace('-', ' ').split(' ')
+        tokens.append((seq, epoch_time))
+print(tokens)
